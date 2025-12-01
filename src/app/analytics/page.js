ba75d4459a4ce766d93e4a8d04b290d579ce169f
eduardo <eduardo.ls@sempreceub.com>
2025-11-30 23:47:50 -0300

    feat: added analytics page with data fetched from UserOrderSummary view

--- src/app/analytics/page.js
+++ src/app/analytics/page.js
@@ -3,15 +3,16 @@ import  LandingPageFooter from "../component/layout/LandingPageFooter";
 import AnalyticsPage from "../component/layout/AnalyticsPage";
 import { getServerSession } from "next-auth";
 import prisma from "@/src/lib/prisma";
+import { authConfig } from "@/auth";
 
 async function getAnalyticsFor(userEmail) {
-  const analytics = await prisma.$queryRaw`SELECT * FROM "UserOrderSummary" WHERE email=${userEmail}`;
-  return analytics
+  const analytics = await prisma.$queryRaw`SELECT * FROM "UserOrderSummary" WHERE email=${userEmail};`;
+  return analytics[0]
 }
 export default async function Cart() {
-  const session = getServerSession()
+  const session = await getServerSession(authConfig)
   const analytics = await getAnalyticsFor(session.user.email)
-    return (
+  return (
         <div className="min-h-screen bg-white text-gray-900 flex flex-col">
           <Navbar />
           <AnalyticsPage analytics={analytics} />
--- src/app/component/layout/AnalyticsPage.jsx
+++ src/app/component/layout/AnalyticsPage.jsx
@@ -3,18 +3,51 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
 export default function AccountDashboard({ analytics }) {
+  console.log(analytics)
   const { status } = useSession();
   if (status === "unauthenticated") {
     redirect("/auth/signin")
   }
-
   return (
     <main className="max-w-2xl min-h-screen flex flex-col items-center mx-auto">
       <div className="w-full flex justify-between my-10">
         <h1 className="text-2xl font-bold">Analytics Page</h1>
       </div>
-      <pre className="w-full bg-gray-200 p-4 rounded break-words whitespace-pre-wrap">
-        {JSON.stringify(analytics, null, 2)}
+      <pre className="w-full bg-gray-200 p-4 rounded  break-words whitespace-pre-wrap">
+        <table class="w-full text-sm text-left rtl:text-right text-body">
+            <thead class="text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default">
+                <tr>
+                    <th scope="col" class="px-6 py-3 font-medium">
+                      Nome
+                    </th>
+                    <th scope="col" class="px-6 py-3 font-medium">
+                      Email
+                    </th>
+                    <th scope="col" class="px-6 py-3 font-medium">
+                      Total gasto
+                    </th>
+                    <th scope="col" class="px-6 py-3 font-medium">
+                      Data da última compra
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-neutral-primary border-b border-default">
+                    <th scope="row" class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                        {analytics.user_name}
+                    </th>
+                    <td class="px-6 py-4">
+                        {analytics.email}
+                    </td>
+                    <td class="px-6 py-4">
+                        {analytics.total_spent}
+                    </td>
+                    <td class="px-6 py-4">
+                      {new Date(analytics.last_order_date).toLocaleDateString()}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
       </pre>
     </main>
   );
