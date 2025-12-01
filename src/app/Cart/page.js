f50af8e209456c123d9491c925040d7da3a8cf4d
eduardo <eduardo.ls@sempreceub.com>
2025-12-01 03:12:55 -0300

    chore: formatted file

--- src/app/Cart/page.js
+++ src/app/Cart/page.js
@@ -1,16 +1,15 @@
 import Navbar from "../component/layout/Navbar";
-import  LandingPageFooter from "../component/layout/LandingPageFooter";
+import LandingPageFooter from "../component/layout/LandingPageFooter";
 import YourCart from "../component/layout/YourCart";
 
-
 export default function Cart() {
-    return (
-        <div className="min-h-screen bg-white text-gray-900 flex flex-col">
-          <Navbar />
-          <YourCart />
-          <footer>
-            <LandingPageFooter />
-          </footer>
-        </div>
-      )
+  return (
+    <div className="min-h-screen bg-white text-gray-900 flex flex-col">
+      <Navbar />
+      <YourCart />
+      <footer>
+        <LandingPageFooter />
+      </footer>
+    </div>
+  );
 }
